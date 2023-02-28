fa75b162e44bf58269a80bdaaa5b08bf870af4e7
Emmanuel Balikuddembe <balixemma@gmail.com>
2023-02-28 20:06:57 +0300

    Implement RTK

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import CartContainer from "./components/CartContainer";
+import CartItem from "./components/CartItem";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+  <main>
+    <Navbar />
+    <CartContainer />
+    <CartItem />
+  </main>
+  );
+}
+export default App;
